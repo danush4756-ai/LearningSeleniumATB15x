b111bc0be3c069f14c5dc954a4c94e363e259ddf
danush4756-ai <danush4756@gmail.com>
2026-02-21 18:12:45 +0530

    Selenium002

---
+++ src/test/java/com/thetestingacademy/Selenium002.java
@@ -0,0 +1,11 @@
+package com.thetestingacademy;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Selenium002 {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://sdet.live");
+    }
+}
