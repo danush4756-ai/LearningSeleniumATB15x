c17007bc2f7e0078e537981770c40ba3cda28f34
danush4756-ai <danush4756@gmail.com>
2026-03-02 14:54:16 +0530

    Close_vs_quit

--- src/test/java/com/thetestingacademy/TestSelenium/Close_vs_quit.java
+++ src/test/java/com/thetestingacademy/TestSelenium/Close_vs_quit.java
@@ -7,6 +7,8 @@ public class Close_vs_quit {
         driver.get("https://google.com");
         Thread.sleep(10000); /*waits for exactly 10 seconds (10,000 milliseconds) before moving
         to the next line of code.*/
+        /*While this works for learning, in a real Selenium Script, using Thread.sleep is generally
+        considered a bad practice. It wastes time*/
         // driver.close(); we usually don't use this in automation
 //        // Close - will close the current tab, not the session (not the all tabs)
         driver.quit();
