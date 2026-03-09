523831652bd377e07bb41ed0b583425d6a810cb9
danush4756-ai <danush4756@gmail.com>
2026-03-09 06:49:52 +0530

    Explicit Waits

--- src/test/java/com/thetestingacademy/Selenium_waits/Task_Explicit_waits.java
+++ src/test/java/com/thetestingacademy/Selenium_waits/Task_Explicit_waits.java
@@ -13,7 +13,8 @@ public class Task_Explicit_waits {
             EdgeDriver driver = new EdgeDriver();
         driver.manage().window().maximize();
             driver.get("https://app.vwo.com");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));/*no waiting happens here it is
+        for future use*/
             WebElement emailInputBox = driver.findElement(By.cssSelector("#login-username"));
             emailInputBox.sendKeys("admin@admin.com");
             WebElement passwordInputBox = driver.findElement(By.cssSelector("[name='password']"));
