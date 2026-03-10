b469bfb21a234aa8f47d07d5f00fc24ded2b6752
danush4756-ai <danush4756@gmail.com>
2026-03-10 21:17:52 +0530

    TestSelenium_Input_Checkbox_Radios

---
+++ src/test/java/com/thetestingacademy/Input_Select_Alerts_Radio/TestSelenium_Input_Checkbox_Radios.java
@@ -0,0 +1,22 @@
+package com.thetestingacademy.Input_Select_Alerts_Radio;
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+public class TestSelenium_Input_Checkbox_Radios {
+    @Description("This is description")
+    @Test
+    public void test_HTMLTags() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://awesomeqa.com/practice.html");
+        WebElement firstname = driver.findElement(By.xpath("//input[@name=\"firstname\"]"));
+        firstname.sendKeys("the testing academy");
+        // RADIO Box
+        WebElement radiobox = driver.findElement(By.id("sex-0"));
+        radiobox.click();
+        // CheckBox
+        driver.findElement(By.id("tool-1")).click();
+    }
+}
