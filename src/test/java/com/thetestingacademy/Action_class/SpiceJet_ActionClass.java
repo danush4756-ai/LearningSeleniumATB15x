9e678626da130c1445964b57c420598f242628e9
danush4756-ai <danush4756@gmail.com>
2026-03-11 11:19:13 +0530

    SpiceJet_ActionClass

---
+++ src/test/java/com/thetestingacademy/Action_class/SpiceJet_ActionClass.java
@@ -0,0 +1,22 @@
+package com.thetestingacademy.Action_class;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+public class SpiceJet_ActionClass {
+    @Test
+    public void test_speicejet() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.spicejet.com/");
+        driver.manage().window().maximize();
+        WebElement origin = driver.findElement(By.xpath("//div[@data-testid=\"to-testID-origin\"]/div/div/input"));
+        WebElement destination = driver.findElement(By.xpath("//div[@data-testid=\"to-testID-destination\"]/div/div/input"));
+        Actions actions = new Actions(driver);
+        actions
+                .moveToElement(origin).click().sendKeys("BLR")
+                .moveToElement(destination).sendKeys("DEL")
+                .build().perform();
+    }
+}
