2b3bfd8bdf21c26525964db81ff3c60a4ac250ab
danush4756-ai <danush4756@gmail.com>
2026-03-11 21:33:57 +0530

    Task_March_9_26

--- src/test/java/com/thetestingacademy/Action_class/Task_March_9_26.java
+++ src/test/java/com/thetestingacademy/Action_class/Task_March_9_26.java
@@ -1,25 +1,34 @@
 package com.thetestingacademy.Action_class;
-
 import com.thetestingacademy.CommonToAll;
 import com.thetestingacademy.CommonToAllEnhanced;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
-
+import java.time.Duration;
 public class Task_March_9_26 extends CommonToAllEnhanced {
     @Test
     public void Booking_com () throws InterruptedException {
-        driver = new EdgeDriver();
+        driver = new FirefoxDriver();
         openBrowser(driver , "https://www.booking.com/");
         WaitElementToBeClickableAndClick(driver, By.xpath("//button[@aria-label='Dismiss sign-in info.']"));
-        Thread.sleep(3000);
         clickElementFound(driver, By.xpath("//a[@id='flights']"));
-        Thread.sleep(3000);
         WaitForVisibilityAndClick(driver , By.xpath("//span[text() ='Going to']"));
-        Thread.sleep(3000);
-        WaitForVisibility(driver, By.xpath("//input[@data-ui-name ='input_text_autocomplete']"));
+        //WaitForVisibility(driver, By.xpath("//input[@data-ui-name ='input_text_autocomplete']"));
+        WebElement input = driver.findElement(By.xpath("//input[@data-ui-name='input_text_autocomplete']"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(input)
+                .keyDown(Keys.SHIFT)
+                .sendKeys(input, "del")
+                .keyUp(Keys.SHIFT)
+                .pause(Duration.ofSeconds(2))
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.ENTER)
+                .build().perform();
+        FindElementAndClick(driver, By.xpath("//span[text() ='Search']"));
     }
 }
