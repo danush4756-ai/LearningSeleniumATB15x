cc49ff39f96aebee09ecd7189a29a03710dc2fb7
danush4756-ai <danush4756@gmail.com>
2026-03-11 09:08:33 +0530

    JavaScript_Alert

---
+++ src/test/java/com/thetestingacademy/Input_Select_Alerts_Radio/JavaScript_Alert.java
@@ -0,0 +1,40 @@
+package com.thetestingacademy.Input_Select_Alerts_Radio;
+import com.thetestingacademy.CommonToAll;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.time.Duration;
+import static com.thetestingacademy.WaitHelpers.waitForAlert;
+import static com.thetestingacademy.WaitHelpers.waitJVM;
+public class JavaScript_Alert extends CommonToAll {
+    @Test
+    public void test_Alerts() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+        System.out.println(driver.getTitle()); /*Title does not live inside the body of the page.
+        It belongs to the entire browser window itself.*/
+        driver.manage().window().maximize();
+        WebElement element = driver.findElement(By.cssSelector("button[onclick='jsAlert()']"));
+        WebElement elementConfirm = driver.findElement(By.xpath("//button[@onclick=\"jsConfirm()\"]"));
+        WebElement elementPrompt= driver.findElement(By.xpath("//button[@onclick=\"jsPrompt()\"]"));
+        //element.click();
+        //elementConfirm.click();
+        elementPrompt.click();
+        waitForAlert(driver,5);
+        Alert alert = driver.switchTo().alert();
+        alert.sendKeys("Darsh");
+        alert.accept();
+//        alert.dismiss();
+        String result = driver.findElement(By.id("result")).getText();
+//        Assert.assertEquals(result,"You successfully clicked an alert");
+//        Assert.assertEquals(result,"You clicked: Ok");
+        Assert.assertEquals(result,"You entered: Darsh");
+        waitJVM(5000);
+        closeBrowser(driver);
+    }
+}
