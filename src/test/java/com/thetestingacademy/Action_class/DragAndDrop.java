e9f6a4204ac45b792ae7ee0450dfb2bec5b645f8
danush4756-ai <danush4756@gmail.com>
2026-03-12 07:29:22 +0530

    Drag and drop

--- src/test/java/com/thetestingacademy/Action_class/DragAndDrop.java
+++ src/test/java/com/thetestingacademy/Action_class/DragAndDrop.java
@@ -1,4 +1,4 @@
-package com.thetestingacademy.Selenium_waits;
+package com.thetestingacademy.Action_class;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
