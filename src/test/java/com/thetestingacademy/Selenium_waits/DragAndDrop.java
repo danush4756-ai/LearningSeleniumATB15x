f785d5a5e97a628e3a1b906ef4ba61f2ad09b4da
danush4756-ai <danush4756@gmail.com>
2026-03-12 07:26:44 +0530

    Drag and drop

---
+++ src/test/java/com/thetestingacademy/Selenium_waits/DragAndDrop.java
@@ -0,0 +1,20 @@
+package com.thetestingacademy.Selenium_waits;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class DragAndDrop {
+        @Test
+                public void DragDrop () {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/drag_and_drop");
+        driver.manage().window().maximize();
+        Actions actions = new Actions(driver);
+        WebElement from = driver.findElement(By.id("column-a"));
+        WebElement to = driver.findElement(By.id("column-b"));
+        actions.dragAndDrop(from,to).build().perform();
+    }
+}
