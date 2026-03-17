30775a2a431b67ab2b62a44ea13196c30e1fcdc0
danush4756-ai <danush4756@gmail.com>
2026-03-17 08:01:24 +0530

    File upload

---
+++ src/test/java/com/thetestingacademy/FileUpload/File_upload.java
@@ -0,0 +1,18 @@
+package com.thetestingacademy.FileUpload;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+public class File_upload {
+    @Test
+    public void upload () {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://awesomeqa.com/selenium/upload.html");
+        WebElement uploadFileInput = driver.findElement(By.id("fileToUpload"));
+        String user_dir_path = System.getProperty("user.dir");
+        System.out.println(user_dir_path);
+        uploadFileInput.sendKeys(user_dir_path + "\\src\\test\\java\\com\\thetestingacademy\\data.json");
+        driver.findElement(By.name("submit")).click();
+    }
+}
